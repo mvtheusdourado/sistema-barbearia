7549d1fd3b2dd27dbf32a58766f695b187510ec8
matheus dourado <matheusdouradolima@gmail.com>
2026-08-07 15:56:13 -0300

    começando front do projeto com react+vite, e middleware pra tratar do cors

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -60,5 +60,19 @@ func main() {
 
 	fmt.Println("Servidor ligado! Acesse: http://localhost:8080/health")
 
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", comCORS(mux))
+}
+func comCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+  return(
+    <div>
+      <h1>Barbearia Matheus</h1>
+      <p>Sistema de Agendamentos</p>
+    </div>
+  )
+}
+
+export default App
